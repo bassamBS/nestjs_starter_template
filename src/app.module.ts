3cf30fbf10159a76147597d7f5bd9dba2a9cdd11
Bassam BEN SALEM <bassam-dev@posteo.net>
2024-05-28 14:12:51 +0200

    🦺 Check env variable before starting app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './env.validation';
 import { HealthCheckModule } from './health-check/health-check.module';
 import { RequestLoggerMiddleware } from './common/middleware/request-logger.middleware';
 
 @Module({
-  imports: [HealthCheckModule],
+  imports: [
+    ConfigModule.forRoot({ validate, isGlobal: true, cache: true }),
+    HealthCheckModule,
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
