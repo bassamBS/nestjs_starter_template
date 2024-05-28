1f29eb628170eac3a8d083d007ecc62fcd1871f1
Bassam BEN SALEM <bassam-dev@posteo.net>
2024-05-28 14:51:44 +0200

    ✨ Add constants related to email

--- src/constant.ts
+++ src/constant.ts
@@ -9,3 +9,9 @@ export const DOCUMENTATION_DESCRIPTION =
   'Documentation swagger du projet Hindbag';
 export const DOCUMENTATION_VERSION = '1.0';
 export const DOCUMENTATION_ENDPOINT = 'hindbag/documentation';
+
+// Email subjects
+export const SUBJECT_INTERNAL_SERVER_ERROR = 'Internal server error';
+
+// Email templates
+export const TEMPLATE_INTERNAL_SERVER_ERROR = 'internal-server-error.ejs';
