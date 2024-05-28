aee12814e2f2f3a5939af53bcdef1eeb210b8600
Bassam BEN SALEM <bassam-dev@posteo.net>
2024-05-28 13:38:22 +0200

    📝 Add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HINDBAG } from './constant';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(HINDBAG);
+
+  const config = new DocumentBuilder()
+    .setTitle('Hindbag')
+    .setDescription('Documentation swagger du projet Hindbag')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: false,
+  });
+
+  SwaggerModule.setup('hindbag/documentation', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
